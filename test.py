9325f690800a3fa7c3ff7a1063c80eeed6c5aae9
qianshuang <2009qianshuang@163.com>
2021-12-31 18:23:35 +0800

    update

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
 
-import Levenshtein
-
-k = "i wodner if i can apply for latin honors"
-query = "I wodner if I cann"
-
-print(k[:len(query)])
-score = Levenshtein.ratio(k[:len(query)], query)
-print(score)
-
-print(Levenshtein.ratio("i wodner if i can", "I wodner if I cann"))
+a = 1
+print(globals())
